d2bba39a42625995cfcae14cb4ad4e0dc4ca9666
kaisergrimlock <s3891987@rmit.edu.vn>
2023-07-02 19:53:05 +0700

    Update header.php

--- src/header.php
+++ src/header.php
@@ -11,7 +11,7 @@
         </div>
         <nav>
             <div><a href="../index.php">HOME</a></div>
-            <div><a href="../src/listings.php">LISTINGS</a></div>
+            <div><a href="/src/listings.php">LISTINGS</a></div>
             <div><a href="contact.php">CONTACT</a></div>
             <div><a href="faq.php">FAQ</a></div>
             <div><a href="testimonial.php">TESTIMONIAL</a></div>
