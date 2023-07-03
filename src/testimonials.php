cd7738a04b78b13248b4683ff72c76df57eb4e76
kaisergrimlock <s3891987@rmit.edu.vn>
2023-07-03 20:34:56 +0700

    added testimonial

---
+++ src/testimonials.php
@@ -0,0 +1,33 @@
+<html>
+    <div class="testimonial">
+        <h1>Our Success Stories</h1>
+        <div class="testimonial-container">
+            <div class="testimonial-box">
+                <img src="/src/img/per-1.jpg" width="150px"/>
+                <div class="testimonial-content">
+                    <h1>Eva E.</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui fuga rem veniam sed modi incidunt dolorum, iure corporis quibusdam sapiente voluptates sunt aliquam praesentium amet quo, ullam minus officiis magni.</p>
+                </div>
+                <button>Read More</button>
+            </div>
+
+            <div class="testimonial-box">
+                <img src="/src/img/per-2.jpg" width="150px"/>
+                <div class="testimonial-content">
+                    <h1>Angela W.</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui fuga rem veniam sed modi incidunt dolorum, iure corporis quibusdam sapiente voluptates sunt aliquam praesentium amet quo, ullam minus officiis magni.</p>
+                </div>
+                <button>Read More</button>
+            </div>
+            
+            <div class="testimonial-box">
+                <img src="/src/img/per-3.png" width="150px"/>
+                <div class="testimonial-content">
+                    <h1>Johny S.</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui fuga rem veniam sed modi incidunt dolorum, iure corporis quibusdam sapiente voluptates sunt aliquam praesentium amet quo, ullam minus officiis magni.</p>
+                </div>
+                <button>Read More</button>
+            </div>
+        </div>
+    </div>
+</html>
